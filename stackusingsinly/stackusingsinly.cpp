49b84c58450eb0191023a7321a022477ffdf69ef
buyaramadhan <ytchochote@gmail.com>
2024-05-10 15:06:22 +0700

    commit 9

--- stackusingsinly/stackusingsinly.cpp
+++ stackusingsinly/stackusingsinly.cpp
@@ -64,4 +64,10 @@ public:
 int main() {
 	Stack stack;
 
-	
+	int choice = 0;
+	int value;
+	while (choice != 5) {
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Peek\n";
+		cout << "4. Exit\n";
