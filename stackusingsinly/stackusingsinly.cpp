a8716b93651d9b33fdc59cc4970f3df78901591e
buyaramadhan <ytchochote@gmail.com>
2024-05-10 15:05:16 +0700

    commit 5

--- stackusingsinly/stackusingsinly.cpp
+++ stackusingsinly/stackusingsinly.cpp
@@ -29,4 +29,14 @@ public:
 		top = newNode; // 4. Update the top pointer to the new node
 		cout << "push value " << value << endl;
 		return value;
-	
+	}
+
+		// Pop operation: Remove the topmost element from the stack
+		void pop() {
+			if (isEmpty()) {
+				cout << "Stack is empty." << endl;
+			}
+			cout << "Popped value: " << top->data << endl;
+			top = top->next; // Update the top pointer to the next node
+		}
+		
