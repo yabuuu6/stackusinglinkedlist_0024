826a2e96a2c799554ac79ffcc7bce46b5f4b7437
buyaramadhan <ytchochote@gmail.com>
2024-05-10 15:04:44 +0700

    commit 4

--- stackusingsinly/stackusingsinly.cpp
+++ stackusingsinly/stackusingsinly.cpp
@@ -19,4 +19,14 @@ private:
 public:
 	Stack() {
 		top = NULL; // Initialize the stack with a null top pointer
+	}
+
+	// Push operation: Insert an element onto the top of the stack
+	int push(int value) {
+		Node* newNode = new Node(); // 1. Allocate memory for the new node
+		newNode->data = value; // 2. assign value
+		newNode->next = top;// 3. Set the next pointer of the new node to the current top node newNode
+		top = newNode; // 4. Update the top pointer to the new node
+		cout << "push value " << value << endl;
+		return value;
 	
