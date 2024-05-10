a74a187fcea2b041842c236578c3d0ab1b25c486
buyaramadhan <ytchochote@gmail.com>
2024-05-10 15:05:33 +0700

    commit 6

--- stackusingsinly/stackusingsinly.cpp
+++ stackusingsinly/stackusingsinly.cpp
@@ -39,4 +39,17 @@ public:
 			cout << "Popped value: " << top->data << endl;
 			top = top->next; // Update the top pointer to the next node
 		}
-		
+		// Peek/Top operation: Retrieve the value of the topmost element without removing it void peek() {
+		void peek() {
+			if (top == NULL) {
+				cout << "List is empty." << endl;
+			}
+			else {
+				Node* current = top;
+				while (current != NULL) {
+					cout << current->data << " " << endl;
+					current = current->next;
+				}
+				cout << endl;
+			}// Return the value of the top node
+	
